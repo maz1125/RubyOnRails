4b54179b9c3f81f15d800ccf32769afcc8fbe1e1
maz1125 <matsuda.daiki.1125.biz@gmail.com>
2018-03-15 08:51:29 +0000

    refs #2 add link and image

--- ruby_lessons/myblog/app/controllers/letters_controller.rb
+++ ruby_lessons/myblog/app/controllers/letters_controller.rb
@@ -3,4 +3,10 @@ class LettersController < ApplicationController
     def index
         @letters = Letter.all.order(created_at:'desc')
     end
+    
+    def show
+        # render plain:params[:id].inspect
+        @letter = Letter.find(params[:id])
+        # render plain:@letter.inspect
+    end
 end
--- ruby_lessons/myblog/config/routes.rb
+++ ruby_lessons/myblog/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   
   resources:posts
   resources:letters
+  root 'letters#index'
 end
